8473a5d7647b4cd8315f08f91a7449d69b10c10a
MihailoVucinic <mihailo.vucinic@stud.fh-campuswien.ac.at>
2022-10-04 18:08:44 +0200

    Task 5 finished.

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -13,26 +13,71 @@ public class App {
     //todo Task 2
     public void helloRobot(){
         // input your solution here
+        System.out.println("0123456789012345678901");
+        System.out.println ("         __" + System.lineSeparator() +
+                " _(\\    |@@|" + System.lineSeparator() +
+                "(__/\\__ \\--/ __" + System.lineSeparator() +
+                "   \\___|----|  |   __" + System.lineSeparator() +
+                "       \\ }{ /\\ )_ / _\\" + System.lineSeparator() +
+                "       /\\__/\\ \\__O (__" + System.lineSeparator() +
+                "      (--/\\--)    \\__/" + System.lineSeparator() +
+                "      _)(  )(_" + System.lineSeparator() +
+                "     `---''---`" + System.lineSeparator());
     }
 
     //todo Task 3
     public void sumOfLiterals(){
         // input your solution here
+        char z = 'Z';
+        int a = 0xface;
+        int b = 012;
+        long c = 80L;
+        float d = 44e-1f;
+        float e = 5.5f;
+        double f = 8.88e1;
+        double g = 99.9;
+
+        int sum = (int) z + a + b +(int) c + (int) d + (int) e + (int) f + (int) g;
+        System.out.println(sum);
     }
 
     //todo Task 4
     public void addTwoNumbers(){
         // input your solution here
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        int sum = x + y;
+        System.out.println(sum);
     }
 
     //todo Task 5
     public void swapTwoNumbers(){
         // input your solution here
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        System.out.println("Before Swap:");
+        System.out.println("x:" + x);
+        System.out.println("y:" + y);
+
+        x=x+y;
+        y=x-y;
+        x=x-y;
+
+        System.out.println("After Swap:");
+        System.out.println("x:" + x);
+        System.out.println("y:" + y);
     }
 
     //todo Task 6
     public void compareTwoNumbers(){
         // input your solution here
+
+
+
     }
 
     //todo Task 7
